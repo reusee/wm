9eada689d060f70ba32411ed04ffacfe2ecfa72f
reus <reusee@gmail.com>
2021-02-27 22:39:34 +0800

    simplify StackByLastFocus; remvoe AdjustWindow

--- window_stack.go
+++ window_stack.go
@@ -7,89 +7,38 @@ import (
 	"github.com/jezek/xgb/xproto"
 )
 
-type AdjustWindow func(windows []*Window, i, j int)
-
-func (def Def) AdjustWindow(
-	conn *xgb.Conn,
-) AdjustWindow {
-	return func(windows []*Window, i, j int) {
-		if j == 0 {
-			// below next
-			sibling := windows[1].XID
-			if sibling == windows[i].XID {
-				ce(xproto.ConfigureWindowChecked(
-					conn, windows[i].XID,
-					xproto.ConfigWindowStackMode,
-					[]uint32{
-						xproto.StackModeBelow,
-					},
-				).Check())
-			} else {
-				ce(xproto.ConfigureWindowChecked(
-					conn, windows[i].XID,
-					xproto.ConfigWindowSibling|
-						xproto.ConfigWindowStackMode,
-					[]uint32{
-						uint32(sibling),
-						xproto.StackModeBelow,
-					},
-				).Check())
-			}
-
-		} else {
-			// above previous
-			sibling := windows[j-1].XID
-			if sibling == windows[i].XID {
-				ce(xproto.ConfigureWindowChecked(
-					conn, windows[i].XID,
-					xproto.ConfigWindowStackMode,
-					[]uint32{
-						xproto.StackModeAbove,
-					},
-				).Check())
-			} else {
-				ce(xproto.ConfigureWindowChecked(
-					conn, windows[i].XID,
-					xproto.ConfigWindowSibling|
-						xproto.ConfigWindowStackMode,
-					[]uint32{
-						uint32(sibling),
-						xproto.StackModeAbove,
-					},
-				).Check())
-			}
-		}
-	}
-}
-
 type StackByLastFocus func()
 
 func (def Def) StackByFocus(
 	get GetWindowsArray,
-	update Update,
-	adjust AdjustWindow,
+	conn *xgb.Conn,
 ) StackByLastFocus {
 	return func() {
+		pt("sort\n")
 
 		windows := get()
 		if len(windows) < 2 {
 			return
 		}
 
-		updated := false
-		sort.Sort(WindowsSorter{
-			windows: windows,
-			less: func(i, j int) bool {
-				return windows[i].LastFocus.Before(windows[j].LastFocus)
-			},
-			swap: func(i, j int) {
-				updated = true
-				adjust(windows, i, j)
-				adjust(windows, j, i)
-			},
+		sort.SliceStable(windows, func(i, j int) bool {
+			return windows[i].LastFocus.Before(windows[j].LastFocus)
 		})
-		if updated {
-			update(def.WindowsArray)
+
+		for i, win := range windows {
+			pt("%v\n", win)
+			if i > 0 {
+				prev := windows[i-1] // prev
+				ce(xproto.ConfigureWindowChecked(
+					conn, win.XID,
+					xproto.ConfigWindowSibling|
+						xproto.ConfigWindowStackMode,
+					[]uint32{
+						uint32(prev.XID),
+						xproto.StackModeAbove,
+					},
+				).Check())
+			}
 		}
 
 	}
